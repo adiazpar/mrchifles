c24378bb5aaf0160385d827049d3565e32d89e89
adiazpar <alexdiaz0923@gmail.com>
2026-02-18 10:24:04 -0500

    Added new migration to commit

---
+++ pb_migrations/1771426888_updated_users.js
@@ -0,0 +1,24 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add field
+  collection.fields.addAt(16, new Field({
+    "hidden": false,
+    "id": "userpinrst01",
+    "name": "pinResetRequired",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "bool"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove field
+  collection.fields.removeById("userpinrst01")
+
+  return app.save(collection)
+})
