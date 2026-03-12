321d9de313a4643e024196e5425297b8da78f619
adiazpar <alexdiaz0923@gmail.com>
2026-03-11 21:30:15 -0500

    feat(icons): add IconArrowLeft

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -807,5 +807,22 @@ export function IconRetiro(props: IconProps) {
   )
 }
 
+export function IconArrowLeft(props: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      {...props}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+    </svg>
+  )
+}
+
 // Backwards compatibility alias
 export const IconEgreso = IconRetiro
